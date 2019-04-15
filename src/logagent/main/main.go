f656fad98b324c915f883afc550ab47beb901490
malinchao <942431368@qq.com>
2019-04-16 06:50:58 +0800

    ok

--- src/logagent/main/main.go
+++ src/logagent/main/main.go
@@ -1,46 +1,45 @@
 package main
 
-
 import (
 	"fmt"
 	"github.com/astaxie/beego/logs"
 	"logagent/kafka"
-	"logagent/tailf")
+	"logagent/tailf"
+)
 
-
-func main(){
+func main() {
 	file_name := "./logcollect.conf"
-	err := loadconf("ini",file_name)
-	if err != nil{
-		fmt.Printf("load conf failed,err:%v",err)
+	err := loadconf("ini", file_name)
+	if err != nil {
+		fmt.Printf("load conf failed,err:%v", err)
 		return
 	}
-	fmt.Println(appconfig.log_path,appconfig.log_level,appconfig.collectconf)
+	fmt.Println(appconfig.log_path, appconfig.log_level, appconfig.collectconf)
 	err = initLogger()
-	if err != nil{
-		fmt.Printf("loadLogs() failed,err:%v",err)
+	if err != nil {
+		fmt.Printf("loadLogs() failed,err:%v", err)
 		return
 	}
-	err = tailf.Inittail(appconfig.collectconf)
-	if err != nil{
-		fmt.Printf("inittail() failed,err:%v",err)
+	collectConf, err := initEtcd(appconfig.etcdAddr, appconfig.etcdKey)
+	if err != nil {
+		logs.Error("etcd init failed,err:%v", err)
+		return
+	}
+	err = tailf.Inittail(collectConf)
+	if err != nil {
+		fmt.Printf("inittail() failed,err:%v", err)
 		return
 	}
 	err = kafka.InitKafka(appconfig.kafka_addr)
-	if err != nil{
-		fmt.Printf("InitKafka() failed,err:%v",err)
+	if err != nil {
+		fmt.Printf("InitKafka() failed,err:%v", err)
 		return
 	}
 	fmt.Println("log system init success")
 
-	err = initEtcd(appconfig.etcdAddr,appconfig.etcdKey)
-	if err != nil{
-		logs.Error("etcd init failed,err:%v",err)
-		return
-	}
 	err = serverrun()
-	if err != nil{
-		logs.Error("serverrun failed,err %v",err)
+	if err != nil {
+		logs.Error("serverrun failed,err %v", err)
 		return
 	}
 
